e8761328c26da0d5326dedbcb8f6bd6750e58ddc
bechernie <bechernie@gmail.com>
2024-10-12 15:12:03 +0200

    initial cdk deploy setup

--- cdk/src/main/java/com/myorg/CdkApp.java
+++ cdk/src/main/java/com/myorg/CdkApp.java
@@ -1,42 +1,32 @@
 package com.myorg;
 
+import dev.stratospheric.cdk.SpringBootApplicationStack;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 public class CdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new CdkStack(app, "CdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        final var accountId = (String) app.getNode().tryGetContext("accountId");
+        Objects.requireNonNull(accountId, "context variable 'accountId' must not be null");
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+        final var region = (String) app.getNode().tryGetContext("region");
+        Objects.requireNonNull(region, "context variable 'region' must not be null");
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        new SpringBootApplicationStack(app, "SpringBootApplication", makeEnv(accountId, region), "docker.io/bechernie/todo-app-v1:latest");
 
         app.synth();
     }
+
+    static Environment makeEnv(String accountId, String region) {
+        return Environment.builder()
+                .account(accountId)
+                .region(region)
+                .build();
+    }
+
 }
 
