b32679644570ac453315528c0c465a1d302b9477
bechernie <bechernie@gmail.com>
2024-10-12 13:29:03 +0200

    reformat

--- application/src/main/java/dev/stratospheric/todoapp/IndexController.java
+++ application/src/main/java/dev/stratospheric/todoapp/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-  @GetMapping(path="/")
-  public String getIndex() {
-    return "index";
-  }
+    @GetMapping(path = "/")
+    public String getIndex() {
+        return "index";
+    }
 
 }
